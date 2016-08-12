e57e28b879bf2eb8dc1fe7231b58b1a642c51a41
dlichterman <dlichterman@gmail.com>
2016-08-12 14:10:30 -0700

    Fix regex

--- forum.php
+++ forum.php
@@ -382,7 +382,7 @@ Type:
 		  $ch = curl_init();
 		  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 		  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		  curl_setopt($ch, CURLOPT_URL,($apiurl . urlencode($twitter_url)));
+		  curl_setopt($ch, CURLOPT_URL,($apiurl . urlencode($twitter_url[0])));
 		  $result=curl_exec($ch);
 		  curl_close($ch);
 		  $json = json_decode($result);
