dca3aaa9c16f7967b76aa567151693b6bf2f2ab2
Daniel Lichterman <dlichterman@gmail.com>
2021-01-16 17:20:43 -0800

    Cleaning up missed comment to delete

--- forum.php
+++ forum.php
@@ -438,8 +438,6 @@ Type:
       {
         $apiurl = 'https://graph.facebook.com/oembed_video?url='
       }
-
-      // /oembed_video?url={url}&access_token={access-token}
       $ch = curl_init();
 		  $ua = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.16 (KHTML, like Gecko) \ Chrome/24.0.1304.0 Safari/537.16';
       $authorization = 'Authorization: Bearer AppID|ClientToken';
