f2a8528a69b720e2312e939a40026590bf6cc0be
optikal <optikalus@users.noreply.github.com>
2022-02-24 09:54:44 -0800

    Updating headers to fix iphone weirdness

--- forum.php
+++ forum.php
@@ -95,11 +95,11 @@ if (isset($_GET['display_mode']) && $_GET['display_mode'] == 1) {
 } 
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">        
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=.5, maximum-scale=1.0" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=.5, shrink-to-fit=no">
   <title><?=$config['title']?></title>
 
   <script language="Javascript" type="text/javascript">
